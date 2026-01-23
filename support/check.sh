eef76833a26c1e1cbd8aad97260efdeff1f14318
Calvin Lee <pounce@integraldoma.in>
2026-01-23 15:03:57 +0000

    chore: rewrite support/check.sh
    This script wasn't running on my machine because egrep isn't 1000 years
    old, so i rewrote it to use only posix shell idioms and (posix) awk. so
    hopefully it will run flawlessly for 1000 years longer

--- support/check.sh
+++ support/check.sh
@@ -1,48 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -eo pipefail
+set -e
 
-# Slightly hacky script to generate _build/all-pages.agda and check it,
+# Beautiful AWK script to generate _build/all-pages.agda and check it,
 # possibly with a specified version of Agda (use the AGDA environment
 # variable).
 
-unsorted="$(mktemp)"
 agda="${AGDA:-agda}"
 
-if ! type "${agda}" &>/dev/null; then
+if ! command -v "${agda}" 2>/dev/null; then
   echo "${agda} is not executable. Either add it to your path or set \$AGDA."
   exit 1
 else
   echo ">>> Checking with ${agda} ($(type "${agda}"))"
 fi
 
-add-module () {
-  echo "$1"                             | \
-    # Remove the leading `src/`, and turn slashes into dots
-    sed -re 's@^src/@@;s@/@.@g'         | \
-
-    # Remove the trailing file extension
-    sed -re 's@(\.lagda\.md|\.agda)$@@' | \
-
-    # Make this into an import
-    sed -re 's@^@open import @'         >> ${unsorted}
+git ls-files --full-name src/ | awk "$(cat<<'EOF'
+BEGIN{
+  num_files = 0;
 }
+/\.lagda\.md$/ || /\.agda$/ {
+  ++num_files;
+  gsub(/^src\//, "");
+  gsub(/\.lagda\.md|\.agda$/, "");
+  gsub(/\//, ".");
+  print "open import " $0;
+}
+END {
+  print ">>> Checking " num_files " modules" > "/dev/stderr";
+}
+EOF
+)" | sort > _build/all-pages.agda
 
-echo ">>> Building _build/all-pages.agda"
-
-git ls-files --full-name src/ | while IFS='' read file; do
-  if echo "${file}" | egrep "l?agda(.md)?$" >/dev/null; then
-    printf '.'
-    add-module "${file}"
-  fi
-done
-
-# Sort the all-pages file to mimic what we would get with a proper
-# build, call the provided executable.
-
-printf '\n>>> Checking %s modules\n' "$(wc -l "${unsorted}" | cut -d' ' -f1)"
-
-sort "${unsorted}" > _build/all-pages.agda
-rm "${unsorted}"
-
-${AGDA:-agda} _build/all-pages.agda --trace-imports=3 ${AGDAFLAGS}
+$agda _build/all-pages.agda --trace-imports=3 ${AGDAFLAGS}
